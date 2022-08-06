e6d27f6b9e5977f4c5efb642841bbe9128720853
tanishilath <77871540+tanishilath@users.noreply.github.com>
2022-08-06 18:37:59 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "TOMORROW";
+const accessCode2 = "WATERMELON";
+const accessCode3 = "BOTTLE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    //text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: always arriving, but never arrives !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    //text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: a green house with red walls!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    //text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint:I have a neck and a cap, but no face !!", 100,270)
+
+}
